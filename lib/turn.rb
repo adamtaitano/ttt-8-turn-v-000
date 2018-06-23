dd1e28a80da92364d4314eaeab1a3d14ac17ddc7
Adam Taitano <adam.taitano@gmail.com>
2018-06-23 01:49:30 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,13 +1,17 @@
-def display_board(board = [" "," "," "," "," "," "," "," "," "])
-  cell = " "
-  seperator = "-----------"
-  bracket = "|"
+def display_board(array = [" "," "," "," "," "," "," "," "," "])
+
+  line = "-----------"
   
-  row_1 = cell + bracket + cell + bracket + cell
-  row_2 = cell + bracket + cell + bracket + cell
-  row_3 = cell + bracket + cell + bracket + cell
+  row_one = " #{array[0]} | #{array[1]} | #{array[2]} "
+  row_two = " #{array[3]} | #{array[4]} | #{array[5]} "
+  row_three = " #{array[6]} | #{array[7]} | #{array[8]} "
   
-  board 
+  puts row_one
+  puts line
+  puts row_two
+  puts line
+  puts row_three
+
 end
 
 def position_taken?(board, index)
