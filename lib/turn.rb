2f6d9eae166b1c35a08733ab45ef9d7052e0bfd5
Adam Taitano <adam.taitano@gmail.com>
2018-06-23 02:09:54 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,7 +19,11 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  
+  if index.between?(0,8)
+    if !position_taken?(board, index)
+      true
+    end
+  end
 end
 
 def input_to_index(input)
