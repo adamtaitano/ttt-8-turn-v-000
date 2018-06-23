cae1213c3deb31c0131428df7e1e46ee2cb2d45a
Adam Taitano <adam.taitano@gmail.com>
2018-06-23 02:14:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,14 @@ def display_board(array = [" "," "," "," "," "," "," "," "," "])
 end
 
 def position_taken?(board, index)
-  if board[index] != " "
+  if board[index] == "O"
+    return true
+  elsif board[index] == "X"
+    return true
+  elsif board[index] != "X"
+    return false
+  elsif board[index] != "O"
+    return false
   end
 end
 
