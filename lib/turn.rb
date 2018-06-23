432cb67b6f53d1b57bd09b287708f8dc32ce32c3
Adam Taitano <adam.taitano@gmail.com>
2018-06-23 01:40:12 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+def display_board(board)
+end
+
+def position_taken?(board, index)
+end
+
+def valid_move?(board, index)
+end
+
+def input_to_index(input)
+end
+
+def move(board,token = "X")
+end
+
+def turn
+end
