7782bdd1ff5fd4ec9022ff911d0afe4ccd58ecf8
Adam Taitano <adam.taitano@gmail.com>
2018-06-23 01:46:59 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,13 @@
 def display_board(board)
+  cell = " "
+  seperator = "-----------"
+  bracket = "|"
+  
+  row_1 = cell + bracket + cell + bracket + cell
+  row_2 = cell + bracket + cell + bracket + cell
+  row_3 = cell + bracket + cell + bracket + cell
+  
+  board 
 end
 
 def position_taken?(board, index)
